0d21b8155b3a5773e354f0cb7ee6de5acb8321ae
Sadeep <sadeepdeshal2@gmail.com>
2025-08-13 23:38:49 +0530

    feat: implement authentication routes and user model, set up database connection

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export const signup = (req, res) => {
+  // Handle user signup
+  res.send("User signed up");
+};
+
+export const login = (req, res) => {
+  // Handle user login
+  res.send("User logged in");
+};
+
+export const logout = (req, res) => {
+  // Handle user logout
+  res.send("User logged out");
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import authRoutes from './routes/auth.route.js';
+import dotenv from 'dotenv';
+import connectDB from './lib/db.js';
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5001, () => {
-  console.log('Server is running on port 5001');
+const PORT=process.env.PORT;
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
