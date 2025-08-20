3d45e8b82f9faffd1a6aefe6d1d611dbfe6f5dcc
Sadeep <sadeepdeshal2@gmail.com>
2025-08-21 02:04:35 +0530

    Add CORS support, update authentication flow, and enhance signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes,Route} from 'react-router-dom'
+import { Routes,Route,Navigate} from 'react-router-dom'
 
 
 import Navbar from './components/Navbar.jsx'
@@ -9,20 +9,46 @@ import SignUpPage from './pages/SignUpPage.jsx'
 import SettingsPage from './pages/SettingsPage.jsx'
 import ProfilePage from './pages/ProfilePage.jsx'
 
+import { useAuthStore } from './store/useAuthStore.js'
+import { useEffect } from 'react'
+
+import {Loader} from 'lucide-react'
+import { Toaster } from 'react-hot-toast'
+
  const App = () => {
+
+  const { authUser,checkAuth,isCheckingAuth } = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+
+  console.log({authUser})
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
   return (
     <div >
 
       <Navbar/>
       <Routes>
 
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
         
+
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+
       </Routes>
+
+      <Toaster />
 
     </div>
   )
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,42 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+
+export const useAuthStore = create((set, get) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true,
+  
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+
+      set({ authUser: res.data });
+      // TODO: Implement socket connection
+      // get().connectSocket();
+    } catch (error) {
+      console.log("Error in checkAuth:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created successfully");
+      // TODO: Implement socket connection
+      // get().connectSocket();
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+}));
