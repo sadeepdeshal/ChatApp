c0b700f6d634bbaf859d70b3ccd357003d495a8b
Sadeep <sadeepdeshal2@gmail.com>
2025-08-21 00:09:57 +0530

    update dependencies and refactor project structure
    - Updated @eslint/js to version 9.33.0 and @types/react to version 19.1.10.
    - Upgraded @vitejs/plugin-react to version 5.0.0 and eslint to version 9.33.0.
    - Updated vite to version 7.1.2.
    - Removed postcss.config.js and tailwind.config.js files.
    - Refactored App component to include routing with react-router-dom.
    - Added Navbar component for navigation.
    - Created new pages: HomePage, LoginPage, SignUpPage, SettingsPage, and ProfilePage.
    - Updated index.css to include daisyui plugin.
    - Wrapped App component with BrowserRouter in main.jsx.
    - Removed unused react.svg asset.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Routes,Route} from 'react-router-dom'
 
-const App = () => {
+
+import Navbar from './components/Navbar.jsx'
+import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage.jsx'
+import SettingsPage from './pages/SettingsPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
+
+ const App = () => {
   return (
-    <div className='text-red-500'>hello</div>
+    <div >
+
+      <Navbar/>
+      <Routes>
+
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        
+      </Routes>
+
+    </div>
   )
 }
 
