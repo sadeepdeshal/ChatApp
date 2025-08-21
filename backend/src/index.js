2efe96d3746cfc642e321e921818a9a843f15c3b
Sadeep <sadeepdeshal2@gmail.com>
2025-08-22 03:16:23 +0530

    App Completed and Ready for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,13 @@ import cookieParser from "cookie-parser";
 import cors from 'cors';
 import {app,server} from './lib/socket.js';
 
+import path from "path";
+
 dotenv.config();
 
 const PORT=process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,6 +24,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
